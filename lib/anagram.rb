4bc72d7e4bde3ef62f65de4fc0514ea7557af587
Angela Stephens <angelastephens317@gmail.com>
2020-06-20 00:54:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,13 +6,8 @@ class Anagram
   end
   
   def match(list_array)
-    @list=list_array
-    new_array =[]
-    self.list.collect do |word|
-      if word.chars.sort == @word.chars.sort
-        new_array << @word
-      end
-    new_array
+    array.select do |word
+    word.split(" ").sort == @word.split("")
   end
   end
 end
